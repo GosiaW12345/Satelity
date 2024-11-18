7aa16a7eac3f2f0976bb9a0612086d20765a2d80
Gosia <Wlodarczyk.Malgorzata.tw@gmail.com>
2024-11-18 19:06:50 +0100

    Kacper pomugł

---
+++ Satelity.py
@@ -0,0 +1,40 @@
+import requests
+import ephem
+import time
+
+class satelity:
+    def __init__(self, adres):
+        self.adres = adres
+        
+    def pobierz_dane(self):
+        self.dane = requests.get(self.adres)
+        self.dane = self.dane.text
+
+    def satelity_lista(self):
+        self.sat_list = []
+        n = 0
+        for line in self.dane.splitlines():
+            if line[0].isalpha():
+                self.sat_list.append({})
+                self.sat_list[n]['nazwa'] = line
+
+            if line[0] == '1':
+                self.sat_list[n]['dane1'] = line
+
+            if line[0] == '2':
+                self.sat_list[n]['dane2'] = line
+                n += 1
+
+    def wypisz(self):
+        for self.i in range(len(self.sat_list)):
+            print(f"{self.i}: {self.sat_list[self.i]['nazwa']}")
+
+#==========================================
+
+AmSat = satelity('http://www.amsat.org/tle/dailytle.txt')
+AmSat.pobierz_dane()
+AmSat.satelity_lista()
+AmSat.wypisz()
+
+
+
