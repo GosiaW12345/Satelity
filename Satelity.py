63379d20f6e9a4fedc2927a9bb2b52abb18b4568
Gosia <Wlodarczyk.Malgorzata.tw@gmail.com>
2025-01-29 20:25:26 +0100

    Program gotowy i działa

--- Satelity.py
+++ Satelity.py
@@ -1,19 +1,19 @@
-from typing import List, Tuple, Any
+from typing import List
 import requests
 import plotly.graph_objects as go
 import ephem
 from datetime import datetime, timedelta, timezone
-
+import pytz
 
 class satellites:
 
     '''
-    klasa jako argument przyjmuje adres do danych TLE
+    klasa jako argument przyjmuje adres url do danych TLE
     metody:
         - download_TLE:
-            metoda czyta plik txt z podanego adresu i zapisuje je w zmiennej dane
+            metoda czyta plik txt z podanego adresu i zapisuje tekst w zmiennej dane
         - list:
-            metoda na podstawie zmiennej dane tworzy listę zawierającą tuple: (nazwa, TLE wiersz1, TLE wiersz2)
+            metoda na podstawie zmiennej dane tworzy listę (sat_list) zawierającą tuple: (nazwa, TLE wiersz1, TLE wiersz2)
         - print_satellite_names:
             metoda wypisuje nazwy satelitów
     '''
@@ -43,8 +43,8 @@ class satellites:
         return self.sat_list
 
     def print_satellite_names (self):
-        for self.i in range(len(self.sat_list)):
-            print(f"{self.i}: {self.sat_list[self.i]['nazwa']}")
+        for i in range(len(self.sat_list)):
+            print(f"{i}: {self.sat_list[i]['nazwa']}")
 
 
 
@@ -53,20 +53,23 @@ def world_map(LAT, LON, set_lat, set_lon, nazwa, set_time, rise_time):
 
     :param LAT: lista (szerokości) na której podstawie jest rysowana trajektoria satelity
     :param LON: lista (długości) na której podstawie jest rysowana trajektoria satelity
-    :param set_lat: punkt (szerokośc) w którym satelita przestaje być widaczny
+    :param set_lat: punkt (szerokość) w którym satelita przestaje być widaczny
     :param set_lon: punkt (długość) w którym satelita przestaje być widaczny
-    :param nazwa:
+    :param nazwa: nazwa satelity
     :param set_time: czas pojawienia się satelity nad horyzontem
     :param rise_time: czas pzniknięcia satelity za horyzont
     :return: None
+
+    Funkcja rysuje fragment trajektori satelity (wtedy kiedy będzie ona widoczna) na mapie, oraz
+    zazmnacza punkt w którym datelita zniknie z horyzontu optycznego
     '''
-    #inicjalizacja wykresu przedstawiającego mapę ziemi
+    # utworzenie obiektu przy pomocy biblioteki plotly, który jest wykresem przedstawiającym mapę ziemi
     fig = go.Figure(go.Scattergeo())
 
-    #ustawienie tytułu
-    fig.update_layout(title=f"Satelita: {nazwa} {set_time} - {rise_time}")
+    # ustawienie tytułu wykresu
+    fig.update_layout(title=f"{nazwa} {set_time} - {rise_time}")
 
-    #ustawienia wyglądu mapy
+    # ustawienia wyglądu mapy
     fig.update_geos(
         projection_type="orthographic",
         showland=True,
@@ -74,7 +77,7 @@ def world_map(LAT, LON, set_lat, set_lon, nazwa, set_time, rise_time):
         showocean=True,
         oceancolor="lightblue")
 
-    #punkt przedstawiający położenie obserwatora
+    # punkt przedstawiający położenie obserwatora
     fig.add_trace(go.Scattergeo(
         name = "twoja lokalizacja",
         lat = [50.45],
@@ -82,7 +85,7 @@ def world_map(LAT, LON, set_lat, set_lon, nazwa, set_time, rise_time):
         mode = 'markers',
         marker = dict(symbol='x', size=5, color='black') ))
 
-    #punkt w którym satelita znika z horyzontu
+    # punkt w którym satelita znika z horyzontu
     fig.add_trace(go.Scattergeo(
         name = "punkt zniknięcia satelity z horyzontu",
         lat = [set_lat],
@@ -90,7 +93,7 @@ def world_map(LAT, LON, set_lat, set_lon, nazwa, set_time, rise_time):
         mode = 'markers',
         marker = dict(symbol='star-diamond-dot', size=7, color='red')))
 
-    #trajektoria
+    # trajektoria
     fig.add_trace(go.Scattergeo(
         name = "trajektoria",
         lat = LAT,
@@ -106,13 +109,14 @@ def prediction(Sat):
 
     :param Sat:
     :return: [visible_points_lat, visible_points_lon, satelite_rise_time, satelite_set_time, satelite_set_lat, satelite_set_lon]
+
     funkcja zwraca kolejno:
         - listę szerokości geograficznych punktów w których satelita będzie widoczny
         - listę długości geograficznych punktów w których satelita będzie widoczny
         - czas pojawienia się satelity nad horyzonrem
         - czas zniknięcia satelity za horyzont
-        - lista (szerokości) na której podstawie jest rysowana trajektoria satelity
-        - lista (długości) na której podstawie jest rysowana trajektoria satelity
+        - punkt (szerokość) w którym satelita przestaje być widaczny
+        - punkt (długość) w którym satelita przestaje być widaczny
     jeżeli satelita wogule nie będzie widoczny w przeciągu 4h to zwracana jest wartość [0,0,0,0,0,0]
 
     Zasada działania funkcji jest następująca:
@@ -122,64 +126,39 @@ def prediction(Sat):
         punkt referencyjny. Następnie zaczynając od czasu referencyjnego sprawdzane są kolejne czas 0,5 min w dół i w górę
         jeżeli dla tych czasów satelita jest widoczny, wspułżędne geograficzne odpowiadające tym czasom dodawane są
         do odpoweidniech list, skrajne czasy zapisywanę są w zmiennych satelite_rise_time i satelite_set_time
-        oznaczają czas pojawienie sie satelity i jego zniknięcia
+        oznaczają czas pojawienie się satelity i jego zniknięcia
     '''
     o = ephem.Observer()
     o.lat = "50.45"
     o.lon = "18.8667"
     o.elevation = 274
 
-    start_time = datetime.now()
+    visible_points_lat = list()
+    visible_points_lon = list()
+    times = list()
 
-    for n in range(30):
-        time = start_time + timedelta(minutes = n*8)
+    start_time = datetime.now(timezone.utc)
+    alt = -1
+    for n in range(120):
+        time = start_time + timedelta(minutes = n)
         o.date = ephem.date(time)
         Sat.compute(o)
         if (Sat.alt) > 0:
-            reference_point = (Sat.sublat/ephem.degree, Sat.sublong/ephem.degree, time)
-            break
+            visible_points_lat.append(Sat.sublat / ephem.degree)
+            visible_points_lon.append(Sat.sublong / ephem.degree)
+            times.append(time)
         else:
-            return [0, 0, 0, 0, 0, 0]
+            if alt>0:   # ten warunek powosuje przerwanie pętli gdy satelita przestaje być widoczny
+                break
+        alt = Sat.alt
 
-    visible_points_lat = []
-    visible_points_lon = []
-
-    visible_points_lat.append(reference_point[0])
-    visible_points_lon.append(reference_point[1])
-
-
-    start_time = reference_point[2]
-
-    satelite_rise_time = None
-    satelite_set_time = None
-    satelite_set_lat = None
-    satelite_set_lon = None
-
-    for n in range(20):
-        time_up = start_time + timedelta(minutes=0.5*(n+1))
-        time_down = start_time - timedelta(minutes=0.5*(n+1))
-
-        o.date = ephem.date(time_up)
-        Sat.compute(o)
-        if (Sat.alt) > 0:
-            visible_points_lat.append(Sat.sublat / ephem.degree)
-            visible_points_lon.append(Sat.sublong / ephem.degree)
-            satelite_set_time = time_up
-            satelite_set_lat = Sat.sublat / ephem.degree
-            satelite_set_lon = Sat.sublong / ephem.degree
-
-        o.date = ephem.date(time_down)
-        Sat.compute(o)
-        if (Sat.alt) > 0:
-            visible_points_lat.append(Sat.sublat / ephem.degree)
-            visible_points_lon.append(Sat.sublong / ephem.degree)
-            satelite_rise_time = time_down
-
-    return [visible_points_lat, visible_points_lon, satelite_rise_time, satelite_set_time, satelite_set_lat, satelite_set_lon]
+    if len(visible_points_lat) == 0:
+        return [0,0,0,0,0,0]
+    else:
+        return visible_points_lat, visible_points_lon, times[0], times[-1], visible_points_lat[-1], visible_points_lon[-1]
 
 
-
-print("\nDostępne grupy satelitów:\n  1) Satelity Amatorskie\n  2) Satelity NOAA\n  2) Międzynarodowa stacja kosmiczna")
+print("\nDostępne grupy satelitów:\n  1) Satelity Amatorskie\n  2) Satelity NOAA\n  3) Międzynarodowa stacja kosmiczna")
 n = input('Podaj grupę satelitów (numer), lub podaj adres do pliku z danymi TLE: ')
 
 if n == '1':
@@ -200,28 +179,29 @@ sat_group.print_satellite_names()
 
 N = input("\nWybierz numery interesujących Cię satelitów (numery odziel przecinkiem) lub wybierz x aby zaznaczyć wszystkie: ")
 
+local_tz = pytz.timezone('Europe/Warsaw')
+
 if N == 'x':
-    longest_visible: list[tuple[Any, Any, datetime, datetime, Any, Any, int]] = []
+    longest_visible: list[tuple[List, List, datetime, datetime, float, float, int]] = []
     for i in range(len(sat_group.sat_list)):
         traced_sat = ephem.readtle(sat_group.sat_list[i]['nazwa'], sat_group.sat_list[i]['dane1'],
                                    sat_group.sat_list[i]['dane2'])
 
         LAT, LON, Rise_time, Set_time, set_lat, set_lon = prediction(traced_sat)
         if Rise_time != 0:
-            if len(longest_visible) == 5:
-                for k in longest_visible:
-                    if k[3] - k[2] < Set_time - Rise_time:
-                        longest_visible.remove(k)
-                        longest_visible.append((LAT, LON, Rise_time, Set_time, set_lat, set_lon, i))
-            else:
-                longest_visible.append((LAT, LON, Rise_time, Set_time, set_lat, set_lon, i))
+            longest_visible.append((LAT, LON, Rise_time, Set_time, set_lat, set_lon, i))
 
-    print("\nZe wszystkich satelitów w wybranej grupie w ciągu najbliższych 4h najdłużej widoczen będą:")
-    for sat in longest_visible:
-        Rise_time = sat[2].strftime('%Y-%m-%d %H:%M')
-        Set_time = sat[3].strftime('%Y-%m-%d %H:%M')
-        print(f"{sat_group.sat_list[sat[6]]['nazwa']}: {Rise_time} - {Set_time}")
-        world_map(sat[0], sat[1], sat[4], sat[5], sat_group.sat_list[i]['nazwa'], Set_time, Rise_time)
+    longest_visible = sorted(longest_visible, key=lambda x: x[3] - x[2], reverse=True)[:5]
+
+    if longest_visible == []:
+        print("Rzaden z wybranych satelitów nie będzie widoczny")
+    else:
+        print("\nZe wszystkich satelitów w wybranej grupie w ciągu najbliższych 4h najdłużej widoczen będą:")
+        for sat in longest_visible:
+            Rise_time = sat[2].astimezone(local_tz).strftime('%Y-%m-%d %H:%M')
+            Set_time = sat[3].astimezone(local_tz).strftime('%Y-%m-%d %H:%M')
+            print(f"{sat_group.sat_list[sat[6]]['nazwa']}: {Rise_time} - {Set_time}")
+            world_map(sat[0], sat[1], sat[4], sat[5], sat_group.sat_list[sat[6]]['nazwa'], Set_time, Rise_time)
 
 
 
@@ -237,10 +217,11 @@ else:
         LAT, LON, Rise_time, Set_time, set_lat, set_lon = prediction(traced_sat)
 
         if Rise_time != 0:
-            Rise_time = Rise_time.strftime('%Y-%m-%d %H:%M')
-            Set_time = Set_time.strftime('%Y-%m-%d %H:%M')
+            Rise_time = Rise_time.astimezone(local_tz).strftime('%Y-%m-%d %H:%M')
+            Set_time = Set_time.astimezone(local_tz).strftime('%Y-%m-%d %H:%M')
             print(f"{sat_group.sat_list[i]['nazwa']}: {Rise_time} - {Set_time}")
             world_map(LAT, LON, set_lat, set_lon, sat_group.sat_list[i]['nazwa'], Set_time, Rise_time)
         else:
             print(f"{sat_group.sat_list[i]['nazwa']} nie będzie widoczny w ciągu najbliższych 4h")
+
 
